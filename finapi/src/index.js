58fd1d1e2cd3e11d512f1bf76f71a83d17606f8e
Emerson Freitas <emersonfreitasn@gmail.com>
2022-03-27 15:22:14 -0500

    criando conta e validando cpf existente

---
+++ finapi/src/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { v4 as uuidV4 } from "uuid";
+
+const app = express();
+
+const customers = [];
+
+app.use(express.json());
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - array
+ */
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response
+      .status(400)
+      .json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidV4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.listen(3333);
